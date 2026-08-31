8ad65b706d7c5f3b05477d89a3c8c867f65fc466
Drashti_Bhanderi03 <25cs003@charusat.edu.in>
2026-08-31 22:15:02 +0530

    add file

---
+++ evenodd.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class evenodd{
+    public static void main(String[] args){
+        Scanner obj = new Scanner(System.in);
+
+        System.out.println("Enter your number: ");
+        int n=obj.nextInt();
+
+        if(n%2==0){
+System.out.println("This number is EVEN");
+        }
+
+        else{
+            System.out.println("Ehis number is odd");
+        }
+
+    }
+
+
+
+
+
+    
+}
